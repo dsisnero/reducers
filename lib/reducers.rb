f626c0ed4e7e03b1f68099efaa0f47561e3cda44
dominic <dsisnero@gmail.com>
2012-12-30 14:05:55 -0700

    initial implementation

--- lib/reducers.rb
+++ lib/reducers.rb
@@ -1,3 +1,49 @@
+require 'stunted'
+
+
 module Reducers
-  VERSION = '0.0.1'
+  extend Stunted::Defn
+
+
+  defn :reducer, ->(coll,xf){
+
+    ->(val,f1,init){
+      coll.reduce(init)[&( xf[f1])]
+    }}
+
+  defn :mapping, ->(f){
+    ->(f1){
+      ->(result,input){
+        f1[result, f[input]]
+      }
+
+    }
+  }
+
+
+
+  defn :filtering, ->(pred){
+    ->(f1){
+      ->(result,input){
+        if pred[input]
+          f1[ result,input]
+        else
+          result
+        end
+      }
+    }
+  }
+
+
+
+  defn :mapcatting, ->(f){
+    ->(f1){
+      ->(result,input){
+        reduce[f1, result, f[input]]
+      }
+    }
+  }
+
+
+
 end
